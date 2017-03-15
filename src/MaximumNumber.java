1aa67a570a8c17418b1a557a25545706d1992722
pallabojha <ojhapallab@gmail.com>
2017-03-15 17:09:39 +0530

    assignment2.3
    assignment2.3

---
+++ src/MaximumNumber.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IIS-KOLKATA
+ */
+public class MaximumNumber {
+ public static void main(String[] args) {
+		int a=91,b=98,c=82,max=0;
+		
+		 max=a;
+		 if(b>max){
+			max=b;
+		}
+			if(c>max){
+				max=c;
+			}
+			System.out.println("higest number is max:"+max);
+
+	}   
+}
